581ca7f52db7155b0d67b0000dbb19b5ec92f509
katafuchix <katafuchix@gmail.com>
2025-07-17 11:08:03 +0900

    Initial Commit

---
+++ OnBoardingExample/OnBoardingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingExampleApp.swift
+//  OnBoardingExample
+//
+//  Created by cano on 2025/07/17.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
